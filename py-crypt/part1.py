1791f74963d713d77d1a00712c9fcc6dd3a240f2
keeperofkey <doddtastic@gmail.com>
2024-02-27 11:53:52 -0700

    python-crypt-project

---
+++ py-crypt/part1.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import string
+
+
+def set_len(key: str) -> str:
+    if len(message) > len(key):
+        key = key * (len(message) // len(key))
+        if len(message) % len(key) != 0:
+            add = len(message) % len(key)
+            key += key[:add]
+        return key
+    elif len(message) < len(key):
+        key = key[: len(message)]
+        return key
+    elif len(message) == len(key):
+        return key
+
+
+def calc_offset(key: str, message: str) -> dict:
+    key = set_len(key)
+    key_arr = []
+    message_arr = []
+    for i in range(len(key)):
+        int_idx = string.ascii_lowercase.index(key[i])
+        key_arr.append(int_idx)
+
+    for i in range(len(message)):
+        int_idx = string.ascii_lowercase.index(message[i])
+        message_arr.append(int_idx)
+
+    mydict = {
+        "key": key_arr,
+        "message": message_arr,
+    }
+    return mydict
+
+
+def encrypt(message: str, key: str) -> str:
+    # apply Vigenere cipher to message using Key
+    off_dict = calc_offset(key, message)
+    new_msg = ""
+    for row in range(len(message)):
+        new_msg += string.ascii_lowercase[
+            (off_dict["key"][row] + off_dict["message"][row]) % 26
+        ]
+    return new_msg
+
+
+def decrypt(message: str, key: str) -> str:
+    # apply Vigenere cipher to message using Key
+    off_dict = calc_offset(key, message)
+    new_msg = ""
+    for row in range(len(message)):
+        new_msg += string.ascii_lowercase[
+            (off_dict["message"][row] - off_dict["key"][row]) % 26
+        ]
+    return new_msg
+
+
+if __name__ == "__main__":
+    message = input("Enter message: ")
+    key = input("Enter key: ")
+    encrypt_message = encrypt(message, key)
+    print(encrypt(message, key))
+    print(decrypt(encrypt_message, key))
